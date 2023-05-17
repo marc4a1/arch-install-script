26369ee7747b22108c6cef946e3e05af905d478d
marc4a1 <115189425+marc4a1@users.noreply.github.com>
2023-05-16 20:49:54 -0700

    Update base.sh

--- base.sh
+++ base.sh
@@ -2,13 +2,13 @@
 
 clear
 
-# Update system clock
+# Update System Clock
 timedatectl set-ntp true
 
-# Show installed devices
+# Show Installed Devices
 lsblk
 
-# Choose drive to install Arch
+# Choose Drive to Install Arch
 echo -n "Enter drive you wish to install Arch on."
 read -r BLOCK_DEVICE
 
@@ -16,20 +16,23 @@ disk=$BLOCK_DEVICE
 swap=${disk}1
 root=${disk}2
 
-# Create disk partitions
+# Create Disk Partitions
+set -xe
 parted -s $disk mklabel msdos
 parted -sa optimal $disk mkpart primary linux-swap 0% 2G
 parted -sa optimal $disk mkpart primary ext4 2G 100%
+parted -s $disk set 1 boot on
 
-# Format partitions
+# Format Partitions
 mkswap -f $swap
 mkfs.ext4 -F $root
 
-# Mount partitions
+# Mount Partitions
 mount $root /mnt
 swapon $swap
 
 # Install Base System
-pacstrap -K /mnt base base-devel linux linux-headers linux-firmware git vim nano networkmanager grub
+pacstrap -K /mnt base base-devel linux linux-firmware
 
+# Generate Fstab
 genfstab -U /mnt >> /mnt/etc/fstab
