71f40d19457ab424208cef707e284647f98894fe
marc4a1 <115189425+marc4a1@users.noreply.github.com>
2023-05-22 21:19:44 -0700

    Update arch-chroot.sh

--- arch-chroot.sh
+++ arch-chroot.sh
@@ -3,15 +3,23 @@
 source user.cfg
 
 # Enter Host and User Details.
+
+echo -n "Enter Timezone (i.e. America/Los_Angeles): "
 read TIMEZONE
+
+echo -n "Enter Hostname: "
 read HOSTNAME
+
+echo -n "Enter Username: "
 read USERNAME
+
+echo -n "Enter Password: "
 read PASSWORD
 
-TIMEZONE=$timezone
-HOSTNAME=$hostname
-USERNAME=$username
-PASSWORD=$password
+timezone=$TIMEZONE
+hostname=$HOSTNAME
+username=$USERNAME
+password=$PASSWORD
 
 # Set Timezone
 ln -sf /usr/share/zoneinfo/$timezone /etc/localtime
@@ -25,15 +33,20 @@ touch /etc/locale.conf
 echo "LANG=en_US.UTF-8" >> /etc/locale.conf
 
 # Configure Host
-
 echo "$hostname" >> /etc/hostname
 echo "127.0.0.1 localhost" >> /etc/hosts
 echo "::1.      localhost" >> /etc/hosts
 echo "127.0.1.1 $hostname.localdomain $hostname" >> /etc/hosts
 
 # Install Grub Bootloader
+pacman -S grub
 grub-install --target=i386-pc $disk
 grub-mkconfig -o /boot/grub/grub.cfg
+
+# Install EFI Bootloader
+# pacman -S grub efibootmgr dosfstools mtools gptfdisk fatresize
+# grub-install --target=x86_64-efi --bootloader-id=grub_uefi --efi-directory=/boot/efi --recheck
+# grub-mkconfig -o /boot/grub/grub.cfg
 
 # Enable System
 systemctl enable NetworkManager
