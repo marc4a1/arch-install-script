482d7c9514e27351b77b2f717efa8e8e156ecb2d
marc4a1 <115189425+marc4a1@users.noreply.github.com>
2023-05-23 13:21:00 -0700

    Update arch-chroot.sh

--- arch-chroot.sh
+++ arch-chroot.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-source user.cfg
-
 # Enter Host and User Details.
 
 echo -n "Enter Timezone (i.e. America/Los_Angeles): "
@@ -26,7 +24,7 @@ ln -sf /usr/share/zoneinfo/$timezone /etc/localtime
 hwclock --systohc
 
 # Set Locale
-sed -i 's/^#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
+sed -i 's/^#en_US.UTF-8/en_US.UTF-8/g' /etc/locale.gen
 locale-gen
 
 touch /etc/locale.conf
