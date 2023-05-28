fdc30c08130fea19d9c53167d43e81e4d2e647d4
marc4a1 <115189425+marc4a1@users.noreply.github.com>
2023-05-28 11:23:20 -0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -59,6 +59,67 @@ cp arch-chroot-uefi.sh config.conf /mnt
 
 arch-chroot /mnt ./arch-chroot-uefi.sh
 
+#!/bin/bash
+
+source config.conf
+
+# Parallel Downloads
+sed -i 's/^#ParallelDownloads/ParallelDownloads/' /etc/pacman.conf
+
+# Enter Host and User Details.
+echo -n "Enter Timezone (i.e. America/Los_Angeles): "
+read timezone
+
+echo -n "Enter Hostname: "
+read hostname
+
+echo -n "Enter Username: "
+read username
+
+echo -n "Enter Password: "
+read password
+
+# Set Timezone
+ln -sf /usr/share/zoneinfo/$timezone /etc/localtime
+hwclock --systohc
+
+# Set Locale
+sed -i 's/^#en_US.UTF-8/en_US.UTF-8/g' /etc/locale.gen
+locale-gen
+
+touch /etc/locale.conf
+echo "LANG=en_US.UTF-8" >> /etc/locale.conf
+
+# Configure Host
+echo "$hostname" >> /etc/hostname
+echo "127.0.0.1 localhost" >> /etc/hosts
+echo "::1.      localhost" >> /etc/hosts
+echo "127.0.1.1 $hostname.localdomain $hostname" >> /etc/hosts
+
+# Install EFI Bootloader
+pacman -Sy
+pacman -S --noconfirm --needed grub efibootmgr
+grub-install --target=x86_64-efi --bootloader-id=GRUB --efi-directory=/boot/efi
+mkdir /boot/grub
+grub-mkconfig -o /boot/grub/grub.cfg
+
+# Install Packages
+pacman -Sy
+pacman -S --noconfirm networkmanager sudo
+
+# Enable System
+systemctl enable NetworkManager
+# systemctl enable fstrim.timer
+
+# Default User
+useradd -m $username
+echo $username:$password | chpasswd
+usermod -aG wheel,audio,video,storage $username
+
+sed -i 's/^# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/g' /etc/sudoers
+
+rm arch-chroot-uefi.sh config.conf
+
 # Unmount and Reboot
 umount -R /mnt
 echo "Remove install media. System will reboot in 10 seconds."
