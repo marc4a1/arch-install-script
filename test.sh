0afdb21825f2ad26e8b47389b6d154ae8373614f
marc4a1 <115189425+marc4a1@users.noreply.github.com>
2023-06-05 10:23:58 -0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -2,10 +2,7 @@
 
 clear
 
-rm config.conf
-touch config.conf
-
-# sed -i 's/^#ParallelDownloads/ParallelDownloads/' /etc/pacman.conf
+sed -i 's/^#ParallelDownloads/ParallelDownloads/' /etc/pacman.conf
 
 # Update System Clock
 timedatectl set-ntp true
@@ -17,38 +14,31 @@ lsblk
 echo -n "Enter drive you wish to install Arch on. (i.e /dev/sda) "
 read -r BLOCK_DEVICE
 
-echo disk=$BLOCK_DEVICE >> config.conf
-
-disk=$BLOCK_DEVICE
+disk=/dev/$BLOCK_DEVICE
 boot=${disk}1
-swap=${disk}2
-root=${disk}3
+root=${disk}2
 
-# Remove Previous mnt and swap
-swapoff $swap
+# Umount mnt for clean install
 umount -R /mnt
 
 # Create Disk Partitions
 set -xe
 parted -s $disk mklabel gpt
 parted -sa optimal $disk mkpart primary fat32 0% 1024M
-parted -sa optimal $disk mkpart primary linux-swap 1024M 2G
-parted -sa optimal $disk mkpart primary ext4 2G 100%
+parted -sa optimal $disk mkpart primary ext4 1024M 100%
 parted -s $disk set 1 esp on
 
 # Format Partitions
 mkfs.fat -F32 $boot
-mkswap -f $swap
 mkfs.ext4 -F $root
 
 # Mount Partitions
 mount $root /mnt
-swapon $swap
 mkdir -p /mnt/boot/efi
 mount $boot /mnt/boot/efi
 
 # Install Base System
-pacstrap -K /mnt base base-devel linux linux-firmware nano vim
+pacstrap -K /mnt base base-devel linux linux-firmware vim
 
 # Generate Fstab
 genfstab -U /mnt >> /mnt/etc/fstab
@@ -110,10 +100,8 @@ usermod -aG wheel,audio,video,storage $username
 
 sed -i 's/^# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/g' /etc/sudoers
 
-rm arch-chroot-uefi.sh config.conf
-
 # Unmount and Reboot
-umount -R /mnt
-echo "Remove install media. System will reboot in 10 seconds."
-sleep 10
-reboot
+#umount -R /mnt
+#echo "Remove install media. System will reboot in 10 seconds."
+#sleep 10
+#reboot
